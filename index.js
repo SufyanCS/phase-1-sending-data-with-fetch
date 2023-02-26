05ae1180de10c56186de8209fccc020d67bb85bd
Sufyan <sufianb.contact@gmail.com>
2023-02-27 02:13:06 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email){
+    const userInfo = {
+        name : name,
+        email : email
+    }
+    const configurationObject = {
+        method : "POST",
+        headers : {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body : JSON.stringify(userInfo)
+    }
+    
+    return (
+        fetch("http://localhost:3000/users", configurationObject)
+        .then (ressponse => ressponse.json())
+        .then (function(object) {
+            let p = document.createElement ("p")
+            p.innerHTML= `${object.name} ${object.email}` 
+            document.querySelector("body").appendChild(p)
+        })
+        .catch(function(error){
+            document.body.innerHTML = error
+        })
+    )
+}
+submitData("Sufyan","s@s.com")
